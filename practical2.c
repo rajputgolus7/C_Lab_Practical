c78cabc036b9d65f8901e82d1ecab36f5192a05c
Golu Singh <golusingh94712@gmail.com>
2026-05-02 16:17:19 +0530

    Create practical2.c

---
+++ practical2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Name: Golu Kumar Singh
+// Roll No: 2510990132
+
+int main() {
+    int x;
+
+    printf("Enter a number: ");
+    scanf("%d", &x);
+
+    printf("The number you entered is: %d", x);
+    return 0;
+}
