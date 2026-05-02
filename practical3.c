873204f309d3ca5038e8aa0f8ca943b821f02e9b
Golu Singh <golusingh94712@gmail.com>
2026-05-02 16:18:46 +0530

    Create practical3.c

---
+++ practical3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Name: Golu Kumar Singh
+// Roll No: 2510990132
+
+int main() {
+    int a = 0, b = 0;
+    int sum;
+
+    // Input
+    printf("Enter two integers separated by space: ");
+    scanf("%d %d", &a, &b);
+
+    // Processing
+    sum = a + b;
+
+    // Output
+    printf("Sum of %d and %d is: %d\n", a, b, sum);
+
+    return 0;
+}
